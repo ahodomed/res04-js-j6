e11648b5aebd1c1e957a15417144805f4236c421
ahodomed <ahodomed@gmail.com>
2023-05-29 15:27:02 +0000

    validation formilaire

---
+++ user/index.js
@@ -0,0 +1,29 @@
+import{
+    User
+} from "./user.js";
+
+
+
+window.addEventListener("DOMContentLoaded", function(){
+    
+    
+})
+
+
+
+//   window.addEventListener("DOMContentLoaded",function(){
+        
+        
+//         let Veggie =new pizza("tomate","fromage","légume");
+//         let Meattie = new pizza("tomate","fromage","viande");
+//         let Fishie = new pizza("tomate","fromage","poisson");
+        
+        
+          
+//     console.log(Veggie);
+//     console.log(Meattie);
+//     console.log(Fishie);
+
+
+//   });
+  
