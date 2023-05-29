1b362cc6468032989f83db3f80eee843b89078f4
ahodomed <ahodomed@gmail.com>
2023-05-29 09:41:51 +0000

    POO-JS

---
+++ index.js
@@ -0,0 +1,21 @@
+import{
+    pizza
+} from "./pizza.js";
+
+
+  window.addEventListener("DOMContentLoaded",function(){
+        
+        
+        let Veggie =new pizza("tomate","fromage","légume");
+        let Meattie = new pizza("tomate","fromage","viande");
+        let Fishie = new pizza("tomate","fromage","poisson");
+        
+        
+          
+    console.log(Veggie);
+    console.log(Meattie);
+    console.log(Fishie);
+
+
+  });
+  
---
+++ pizza.js
@@ -0,0 +1,20 @@
+ class pizza {
+    name;
+    ingredient1;
+    ingredient2;
+    ingredient3;
+
+    constructor(name, ingredient1,ingredient2, ingredient3){
+        this.name=name;
+        this.ingredient1=ingredient1;
+        this.ingredient2=ingredient2;
+        this.ingredient3=ingredient3;
+    }
+    
+}
+
+export{
+    pizza
+};
+
+  
