a26b2fe402edf51a69e4303b6a2dfd3c8b31bb0b
ahodomed <ahodomed@gmail.com>
2023-05-30 09:23:14 +0000

    suite validation formulaire

--- user/users.js
+++ user/users.js
@@ -19,44 +19,203 @@ class User{
         this.biography = biography;
         
     }
+    
+   validateFirstName()
+   {
+            let myform = document.querySelector("form");
+            
+            let myRegex = /^[A-Za-z][A-Za-z_]+$/;
+            
+            
+            myform.addEventListener("submit",function()
+        {
+            
+                     let fname = document.querySelector("#imput1");
+            
+                    if(fname.value.trim() == "")
+                    {
+                
+                      event.preventDefault();
+                      
+                    //  let myerror = document.querySelector("#error");
+                    //  myerror.innerHTML="ce champ est requis";
+                    //  myerror.style.color = "red";
+
+                
+                    }else if(myRegex.test(fname.value)==false)
+                    {
+                
+                                 event.preventDefault();
+
+                     }else{
+                            
+                
+                           }
+            
+
+        });
+        
+    }
+    
+    
+    validateLastName()
+    {
+         let myform = document.querySelector("form");
+            
+            let myRegex = /^[A-Za-z][A-Za-z_]+$/;
+            
+            myform.addEventListener("submit",function()
+        {
+            
+                    let lname = document.querySelector("#imput2");
+            
+                    if(lname.value.trim() == "")
+                    {
+                
+                         event.preventDefault();
+                
+                    }else if(myRegex.test(lname.value)==false)
+                    
+                    {
+                
+                              event.preventDefault();
+
+                     }else
+                     
+                        {
+                            
+                
+                        }
+            
+
+        });
+         
+
+        
+    }
+    
+    
+    validateNickName()
+    {
+          let myform = document.querySelector("form");
+            
+            let myRegex ="^[A-Za-z][A-Za-z0-9_]$";
+            
+            myform.addEventListener("submit",function()
+        {
+            
+                    let nname = document.querySelector("#imput3");
+            
+                    if(nname.value.trim() == "")
+                    {
+                
+                         event.preventDefault();
+                
+                    }else if(myRegex.test(nname.value)==false)
+                    
+                    {
+                
+                              event.preventDefault();
+
+                     }else
+                     
+                        {
+                            
+                
+                        }
+            
+
+        });
+    }
+    
+    validateEmail()
+    {
+          
+         let myform = document.querySelector("form");
+            
+            let myRegex =("#^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#");
+            
+            myform.addEventListener("submit",function()
+        {
+            
+                    let email = document.querySelector("#imput4");
+            
+                    if(email.value.trim() == "")
+                    {
+                
+                         event.preventDefault();
+                
+                    }else if(myRegex.test(email.value)==false)
+                    
+                    {
+                
+                              event.preventDefault();
+
+                     }else
+                     
+                        {
+                            
+                
+                        }
+            
+
+        });
+    }
+    
+    
+    validatePassword()
+    {
+            let myform = document.querySelector("form");
+            
+            let myRegex =("#^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#");
+            
+            myform.addEventListener("submit",function()
+        {
+            
+                    let pass = document.querySelector("#imput5");
+                    let cpass = document.querySelector("#imput6");
+                    
+            
+                    if(pass.value.trim() == "")
+                    {
+                
+                         event.preventDefault();
+                
+                    }else if(myRegex.test(pass.value)==false)
+                    
+                    {
+                
+                              event.preventDefault();
+
+                     }else
+                     
+                        {
+                            
+                
+                        }
+            
+
+        });
+    }
+    
+    validateBiography()
+    {
+           let myform = document.querySelector("form");
+            myform.addEventListener("submit",function(){
+            event.preventDefault();
+        });
+        
+    }
+
+
+validate()
+{
+    
+    
+}
 }
 
 export{
     User
 };
 
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-//  class pizza {
-//     name;
-//     ingredient1;
-//     ingredient2;
-//     ingredient3;
-
-//     constructor(name, ingredient1,ingredient2, ingredient3){
-//         this.name=name;
-//         this.ingredient1=ingredient1;
-//         this.ingredient2=ingredient2;
-//         this.ingredient3=ingredient3;
-//     }
-    
-// }
-
-// export{
-//     pizza
-// };
-
-  
